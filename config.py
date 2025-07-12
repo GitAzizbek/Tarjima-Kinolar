93710c4698549807ef299be04073877bffd23bed
Azizbek Aliyev <159805309+GitAzizbek@users.noreply.github.com>
2025-07-12 09:05:22 +0500

    Updated channel

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "7875373191:AAElS9cwUMFb-nRbvRgZFkYyoGlag0kvsxA"
-CHANNEL_USERNAME = "@kdkdkdkdksss"  
+CHANNEL_USERNAME = "@ksjdkasjdksdhsakjdkjksdasdksadjs"  
 ADMIN_USERNAMES = ["@dasturchiazizbek"]
 ADMIN_IDS = [7112971281, 959868854]
 MOVIE_JSON_PATH = "data/movies.json"
